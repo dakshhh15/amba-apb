ab1c3e4e6cfa4928e7db93d5cbb15675664d6704
Daksh Hanswal <dakshhanswal@gmail.com>
2025-04-24 17:02:28 +0530

    Create packet.sv

---
+++ uvm/packet.sv
@@ -0,0 +1,20 @@
+//import uvm_pkg::*;
+//`include "uvm_macros.svh"
+
+package pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  //`include "design.sv"
+  `include "sequence_item.sv"
+  `include "sequencer.sv"
+  `include "sequence.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "agent.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+  `include "test.sv" 
+  //`include "test_2.sv"
+
+endpackage : pkg
