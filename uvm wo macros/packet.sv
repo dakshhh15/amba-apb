e9dcf6de0bd59bcbf398668212b3fcc230c5d011
Daksh Hanswal <dakshhanswal@gmail.com>
2025-04-24 17:07:12 +0530

    Create packet.sv

---
+++ uvm wo macros/packet.sv
@@ -0,0 +1,18 @@
+package pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  //`include "design.sv"
+  `include "sequence_item.sv"
+  `include "sequencer.sv"
+  `include "sequence.sv"
+  `include "driver.sv"
+  `include "coverage.sv"
+  `include "monitor.sv"
+  `include "agent.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+  `include "test.sv" 
+  //`include "test_2.sv"
+
+endpackage : pkg
