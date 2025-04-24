75f1ddae9ebe298f8483cd008cf56ada0189e477
Daksh Hanswal <dakshhanswal@gmail.com>
2025-04-24 18:03:24 +0530

    Create package.sv

---
+++ uvm design 2/package.sv
@@ -0,0 +1,17 @@
+package pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  `include "sequence_item.sv"
+  `include "reset_seq.sv"
+  `include "main_seq.sv"
+  `include "sequencer.sv"
+  `include "coverage.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "agent.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+  `include "test.sv" 
+
+endpackage : pkg
